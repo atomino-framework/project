b0de33c6ed85a90e2b2fb66925575d50172211ff
Gergely Laborci <gergely@laborci.hu>
2021-05-21 12:16:36 +0200

    ini

--- app/etc/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php return [
-	"database"                 => ["dsn" => "mysql:host=localhost;dbname=atomino;user=root;password=root;charset=UTF8"],
-	"auth"                     => ["jwt-key" => "my-awesome-secret-key"],
-	"attachment-entity-plugin" => ["img" => ["secret" => "quantumbits"]],
-	"smart-responder"          => ["debug" => true],
-	"log-handlers"             => ["sql" => ["level" => \Monolog\Logger::DEBUG]],
-];
--- dev/config/config.php
+++ dev/config/config.php
@@ -1,5 +1,5 @@
 <?php return array_replace_recursive(
 	["settings"=>[]],
 	...array_map(fn($file) => include $file, glob(__DIR__ . "/config/*.php")),
-	...array_map(fn($file) => include $file, glob(__DIR__ . "/../../app/etc/config/*.php")),
+	...[\Atomino\readini(__DIR__.'/../../atomino.ini')],
 );
