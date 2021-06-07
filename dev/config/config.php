74505632f7254dacccdee1a7ee34e065e58c8581
Gergely Laborci <gergely@laborci.hu>
2021-06-07 11:45:44 +0200

    settings separated from loader

--- dev/config/config.php
+++ dev/config/config.php
@@ -1,5 +1,4 @@
 <?php return array_replace_recursive(
-	["settings"=>[]],
 	...array_map(fn($file) => include $file, glob(__DIR__ . "/config/*.php")),
 	...[\Atomino\readini(__DIR__.'/../../atomino.ini')],
 );
