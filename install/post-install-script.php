e7fd365ea32ad41448edc57c17ad52e63f558005
Gergely Laborci <gergely@laborci.hu>
2021-06-29 20:24:34 +0200

    installer fix

--- install/post-install-script.php
+++ install/post-install-script.php
@@ -5,7 +5,7 @@ mkdir($projectRoot.'/.my-repos');
 
 // copy / create base files
 
-copy($projectRoot . '/resources/atomino.ini', $projectRoot . '/atomino.ini');
+copy($projectRoot . '/assets/atomino.ini', $projectRoot . '/atomino.ini');
 unlink($projectRoot . '/.gitignore');
 rename($projectRoot . '/install/.gitignore.dist', $projectRoot . '/.gitignore');
 
