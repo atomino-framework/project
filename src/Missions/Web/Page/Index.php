dd693f34a68a09b821154f1d45ed815aeed83c3c
Gergely Laborci <gergely@laborci.hu>
2021-06-30 13:13:12 +0200

    mobilized :D

--- src/Missions/Web/Page/Index.php
+++ src/Missions/Web/Page/Index.php
@@ -5,16 +5,11 @@ use Atomino\Mercury\Responder\Smart\Attributes\{Cache, Args, CSS, JS, Init};
 use Symfony\Component\HttpFoundation\Response;
 use function Atomino\debug;
 
-#[Init( 'web', 'index.twig' )]
+#[Init( 'web', 'index.twig', 'mobile.twig' )]
 #[Args( title: 'Atomino' )]
 #[Cache( 0 )]
 class Index extends SmartResponder{
 	public array $users;
-	protected function prepare(Response $response){
-		if($this->request->attributes->get('isMobile')){
-			$this->template = 'mobile.twig';
-			$this->smart['title'] = "Atomino Mobile";
-		}
-	}
+	protected function prepare(Response $response){	}
 }
 
