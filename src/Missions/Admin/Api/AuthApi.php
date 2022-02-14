662c8a339198f2e58fb6514f5383c44e6a0139c5
Gergely Laborci <gergely@laborci.hu>
2022-02-14 15:28:57 +0100

    admin authorization added

--- src/Missions/Admin/Api/AuthApi.php
+++ src/Missions/Admin/Api/AuthApi.php
@@ -4,6 +4,7 @@ use Application\Entity\User;
 use Atomino\Carbon\Entity;
 
 class AuthApi extends \Atomino\Gold\AuthApi {
+	public function authorize(Entity|User $user): bool { return $user->hasRole(User::ROLE_ADMIN); }
 	public function getAuthenticated(): Entity|User|null { return User::getAuthenticated(); }
 	public function getUserName(Entity|User $user): string { return $user->name; }
 	public function getUserAvatar(Entity|User $user): string|null { return $user->avatar?->first?->image->crop(64, 64)->png; }
